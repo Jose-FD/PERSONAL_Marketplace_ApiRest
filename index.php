c82529ff75977266d547dd09442d480825333560
Jose-FD <jf153413@gmail.com>
2022-04-15 18:54:25 -0500

    APIREST

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+    /*=============================================
+        TODO: Mostrar errores
+    =============================================*/
+
+    ini_set('display_errors', 1);
+    ini_set("log_errors", 1);
+    ini_set("error_log",  "C:/xampp/htdocs/JR/PERSONAL_Marketplace_ApiRest/php_error_log");
+
+    /*=============================================
+        TODO: CORS
+    =============================================*/
+
+    /* TODO: Permitir el acceso de otro origen */
+
+    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+    header('content-type: application/json; charset=utf-8');
+
+    /*=============================================
+        TODO: Requerimientos
+    =============================================*/
+
+    require_once "controllers/routes.controller.php";
+
+    $index = new RoutesController();
+    $index -> index();
+
+?>
